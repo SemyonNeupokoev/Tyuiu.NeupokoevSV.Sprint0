9fd2a78919b28883fbdb416f46ebd778fa8c8006
semyon <semyon_neupokoev@mail.ru>
2025-09-10 13:16:58 +0300

    Task1

---
+++ Tyuiu.NeupokoevSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Семён";
+        int age = 18;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
